6aba8861d011ea100b727b5a7e5d832a6add5ae5
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-06-30 21:36:24 -0300

    basic-structure

---
+++ a03-front-integral-revisao-css/classe-02/src/App.js
@@ -0,0 +1,20 @@
+import Card from './components/card';
+
+import './App.css'
+
+function App() {
+
+  return (
+    
+    <div className="App">
+      <div className="conteiner">
+
+        <Card />
+             
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
---
+++ a03-front-integral-revisao-css/classe-02/src/components/card/index.js
@@ -0,0 +1,19 @@
+
+import './style.css'
+
+export default function Card() {
+
+  return (
+
+    <div className="card">
+
+      <img src={ {} } alt='imagem logo' />
+
+      <h2>{ }</h2>
+      <p>{ }</p>
+
+      <button>{ }</button>
+
+    </div>
+  )
+}
