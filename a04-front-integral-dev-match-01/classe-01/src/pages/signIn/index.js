77f552c3c6559d00df804e1545873df547633236
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-07-07 17:42:42 -0300

    login-form

--- a04-front-integral-dev-match-01/classe-01/src/pages/signIn/index.js
+++ a04-front-integral-dev-match-01/classe-01/src/pages/signIn/index.js
@@ -1,9 +1,47 @@
+import './styles.css'
+import { Link } from 'react-router-dom'
 
 
 export default function SignIn() {
 
   return (
-    
-    <h1>SignIn</h1>
+    <div className="conteinerForm">
+
+      <form className="formLogin">
+
+        <div className="textCenter mb1rem">
+
+          <h1>Entrar</h1>
+          <Link to="/register">Cadastre-se</Link>
+
+        </div>
+
+        <div className="flexColunm">
+
+          <label htmlFor="email" >E-mail</label>
+          <input id="email" type="text" placeholder="Digite seu email" />
+          
+        </div>
+
+        <div className="flexColunm">
+
+          <label htmlFor="password" >Senha</label>
+          <input id="password" type="password" placeholder="Digite sua senha" />
+
+          <button className="btnDarkBlue">Entrar</button>
+          
+        </div>
+
+        <div>
+
+          <input type="checkbox" value="Lembrar-me" name="remember" />
+          <span htmlFor="remember" >Lembrar-me</span>
+
+        </div>
+
+        <Link to="/forgotpassword" className="my1rem">Esqueci minha senha</Link> <br/>
+        <span className="lightLabel" >@2021 Todos os direitos reservados Tarcisio wesley</span>
+      </form>
+    </div>
   )
 }
