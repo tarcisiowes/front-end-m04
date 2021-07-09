08660d8bf5b7ff2028a7e2cd91f153b1e5227eea
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-07-09 16:39:01 -0300

    register-page

--- a04-front-integral-dev-match-01/classe-01/src/pages/signUp/index.js
+++ a04-front-integral-dev-match-01/classe-01/src/pages/signUp/index.js
@@ -1,9 +1,74 @@
+import './styles.css'
+import { Link } from 'react-router-dom'
 
 
 export default function SignUp() {
 
   return (
-    
-    <h1>SignUp</h1>
+    <div className="conteinerForm ">
+
+      <form className="formRegister form">
+
+        <div className="textCenter mb1rem">
+
+          <h1>Cadastre-se</h1>
+          <Link to="/login">Entrar</Link>
+
+        </div>
+
+        <div className="bodyRegister">
+          <div className="bodyregister">
+            <div className="flexColunm">
+
+              <label htmlFor="name" >Nome</label>
+              <input id="name" type="text" placeholder="Digite seu nome" />
+              
+            </div>
+
+            <div className="flexColunm">
+
+              <label htmlFor="cep" >CEP</label>
+              <input id="cep" type="text" placeholder="Digite seu cep" />
+              
+            </div>
+
+            <div className="flexColunm">
+
+              <label htmlFor="password" >Senha</label>
+              <input id="password" type="password" placeholder="Digite sua senha" />
+
+            </div>
+            
+          </div>
+
+          <div className="bodyregister">
+            <div className="flexColunm">
+
+              <label htmlFor="email" >E-mail</label>
+              <input id="email" type="text" placeholder="Digite seu email" />
+              
+            </div>
+
+            <div className="flexColunm">
+
+              <label htmlFor="city" >Cidade</label>
+              <input id="city" type="text" placeholder="Digite seu cidade" />
+              
+            </div>
+
+            <div className="flexColunm">
+
+              <label htmlFor="confirmPassword" >Confirme sua senha</label>
+              <input id="confirmPassword" type="password" placeholder="Digite sua senha novamente" />
+
+            </div>
+            
+          </div>
+        </div>
+
+        <button className="btnDarkBlue">Cadastrar</button>
+        <span className="lightLabel" >@2021 Todos os direitos reservados Tarcisio wesley</span>
+      </form>
+    </div>
   )
 }
